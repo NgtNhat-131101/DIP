49e8b942b42274830f57cdd3728c11acc4a36a77
NgtNhat-131101 <nguyentiennhat2k1@gmail.com>
2023-12-06 08:47:59 +0700

    representation

--- kmeans_library.py
+++ kmeans_library.py
@@ -1,13 +1,24 @@
 from utils import create_data, visualize
 import warnings
 from sklearn.cluster import KMeans
+from kmeans import KMeanClustering
 
 
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
     data, label = create_data()
     kmeans = KMeans(n_clusters=3, random_state=0).fit(data)
-    print('Centers found by scikit-learn:')
+    print('Centers found by Scikit-Learn:')
     print(kmeans.cluster_centers_)
+
+    print("==============================================================");
+    model = KMeanClustering()
+    labels, centroids = model.fit(data, iterations=300)
+    print("Final Centroids without Scikit-Learn: ")
+    print(centroids)
+
+    model = KMeanClustering(k = 3);
+
     pred_label = kmeans.predict(data)
     visualize(data, label, kmeans.cluster_centers_)
+
